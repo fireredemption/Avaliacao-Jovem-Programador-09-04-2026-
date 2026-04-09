50645087fef50d6f12e52c9c6494cfc384820fe0
Rodrigo Oliveira (Fire) <rodrigo.proxyfire@gmail.com>
2026-04-09 08:51:24 -0300

    Questão 1: Avaliação Jovem Programador (09/04/26)

---
+++ AvaliaçãoQuestão1.java
@@ -0,0 +1,33 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class AvaliaçãoQuestão1 {
+
+	public static void main(String[] args) {
+
+		        Scanner input = new Scanner(System.in);
+
+		        double lado1, lado2, lado3;
+
+		        System.out.print("Digite o primeiro lado: ");
+		        lado1 = input.nextDouble();
+
+		        System.out.print("Digite o segundo lado: ");
+		        lado2 = input.nextDouble();
+
+		        System.out.print("Digite o terceiro lado: ");
+		        lado3 = input.nextDouble();
+
+		        // Classificação
+		        if (lado1 == lado2 && lado2 == lado3) {
+		            System.out.println("Triângulo: Equilátero");
+		        } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+		            System.out.println("Triângulo: Isósceles");
+		        } else {
+		            System.out.println("Triângulo: Escaleno");
+		        }
+
+		        input.close();
+		    }
+		}	
